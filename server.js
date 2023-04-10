daaaf319d1c2d46bf153414fb9e2c652506b6dc6
hamzaoda <97050670+hamzaoda@users.noreply.github.com>
2023-04-10 15:43:10 +0400

    Update URL

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://hamza-s-shop-production.up.railway.app"],
     credentials: true
 }));
 
